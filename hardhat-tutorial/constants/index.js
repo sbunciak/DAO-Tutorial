7b0d6caa5b21cf99fd5208f21f8972283a1175b0
Stefan Bunciak <pipistik.bunciak@gmail.com>
2022-10-14 10:50:53 +0200

    Finished DAO tutorial

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS = "0x10bBF14ef10658032ada2b387b655Dac980f250A";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
